63698933a1d751f719c4eac7a5f2e4f350d32cba
Roger Wolf <roger.wolf@kit.edu>
2023-03-15 09:52:17 +0100

    increase functionality

--- Run-2-Mh125/update_MSSM_limits_mh125.py
+++ Run-2-Mh125/update_MSSM_limits_mh125.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
 from sys import argv
 from os import system 
-system("root -l -b -q -e \".x MSSM_limits_mh125.C+\"")
-system("pdftoppm MSSM_limits_mh125.pdf MSSM_limits_mh125 -png -cropbox")
+
+if len(argv)>1: 
+    if argv[1]=="-c" or argv[1]=="--clean":
+        system("rm *.pcm *.d *.so *.pdf *.png")
+else:
+    system("root -l -b -q -e \".x MSSM_limits_mh125.C+\"")
+    system("pdftoppm MSSM_limits_mh125.pdf MSSM_limits_mh125 -png -cropbox")
+    system("mv MSSM_limits_mh125-1.png MSSM_limits_mh125.png")
+
--- Run-2-hMSSM/update_MSSM_limits_hMSSM.py
+++ Run-2-hMSSM/update_MSSM_limits_hMSSM.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
 from sys import argv
 from os import system 
-system("root -l -b -q -e \".x MSSM_limits_hMSSM.C++\"")
-system("pdftoppm MSSM_limits_hMSSM.pdf MSSM_limits_hMSSM -png -cropbox")
+
+if len(argv)>1: 
+    if argv[1]=="-c" or argv[1]=="--clean":
+        system("rm *.pcm *.d *.so *.pdf *.png")
+else:
+    system("root -l -b -q -e \".x MSSM_limits_hMSSM.C+\"")
+    system("pdftoppm MSSM_limits_hMSSM.pdf MSSM_limits_hMSSM -png -cropbox")
+    system("mv MSSM_limits_hMSSM-1.png MSSM_limits_hMSSM.png")
